c2acd5e9395420e03b6a75a358961d93a67c14c4
DELL <deepthigalla2525@gmail.com>
2025-02-04 18:47:06 +0530

    pattern

--- startproject/src/startproject/Pattern3.java
+++ startproject/src/startproject/Pattern3.java
@@ -9,7 +9,7 @@ public class Pattern3 {
 		{
 			for(int j=0;j<i;j++)
 			{
-				System.out.print("*");
+				System.out.print("*  ");
 			}
 			System.out.println("");
 		}
@@ -17,7 +17,7 @@ public class Pattern3 {
 		{
 			for(int j=0;j<n-i;j++)
 			{
-				System.out.print("*");
+				System.out.print("*  ");
 			}
 			System.out.println("");
 		}
